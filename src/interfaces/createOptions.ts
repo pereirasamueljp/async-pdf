f3b79422cde91e6f15d8b8ec8bdc6316b1eade6b
Samuel Pereira <pereirasamueljp@gmail.com>
2022-05-05 14:26:46 -0300

    Fix the createOptions - Font

--- src/interfaces/createOptions.ts
+++ src/interfaces/createOptions.ts
@@ -9,5 +9,5 @@ export interface PDFCreateOptions {
     orientation: PDFPageOrientationTypes;
     pageSize: PDFPageSize;
     pageSpacing: PDFPageSpacing;
-    font: PDFFontTypes | string;
+    font: PDFFontTypes;
 }
